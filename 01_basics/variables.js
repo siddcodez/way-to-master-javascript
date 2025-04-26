4ebe59d6c3fddf71a440909c87541ac2fc287715
siddharth <progammersidd@gmail.com>
2025-04-26 08:35:38 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,8 @@
+const rollNumber = 124323
+let stName = "sidd"
+var stEmail = "sidd@gmail.com"// we can't use var because block scope or functional scope. That's why we use let for sure.
+let stclass
+
+// rollNumber = 12321 // here we can't change the value of const.
+
+console.table([rollNumber,stEmail,stName,stclass])
