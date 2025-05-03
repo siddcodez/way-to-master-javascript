2f1e6359e10661e7f2da7bd8513591500892619d
siddharth <progammersidd@gmail.com>
2025-05-03 11:45:47 +0000

    coconversion.js

---
+++ 01_basics/Conversion.js
@@ -0,0 +1,16 @@
+// let score = "33"
+
+// console.log("33 is ", typeof(score))
+
+// let valueInNumber = Number(score)
+
+// console.log("33 is ",typeof(valueInNumber))
+
+
+let score2 = "33abc"
+
+console.log("33abc is ", typeof(score2))
+
+let valueInNumber1 = Number(score2)
+
+console.log("33abc is ",typeof(valueInNumber1))
